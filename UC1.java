20f222f14468b1a9cb89973ba6c8788cacb9f9bb
Nivedita-PA <npal4661@gmail.com>
2021-05-05 19:02:08 +0530

    [Nivedita]: added the LinkedList for three Integer elements

---
+++ UC1.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+	List<Integer> ll = new LinkedList<>();
+	ll.add(56);
+	ll.add(30);
+	ll.add(70);
+
+	System.out.println(ll);
+	}
+
+}
